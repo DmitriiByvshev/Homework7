27cd99a8063524ad43ceaad02f91bcc5aa0b0cb9
DmitriiByvshev <84013805+DmitriiByvshev@users.noreply.github.com>
2021-12-28 04:28:07 +0300

    Homework8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
         System.out.println(CityNumber.sendCityIdRequest("Saint Petersburg"));
         System.out.println(WeatherDays.sendTempRequest("295212"));
+        DbHandler dbHandler = new DbHandler();
     }
 
 }
